0e30e80ad6b7834adf28a3609d113f6b5ff84174
MathiasDWaterlaw <mathiasdellosa@gmail.com>
2023-12-29 03:59:08 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const card = document.getElementById("card");
+const thanksCard = document.getElementById("thanks-card");
+const submitButton = document.querySelector(".btn-submit");
+const ratingSelection = document.querySelectorAll(".btn");
+const finalRating = document.getElementById('final-rating');
+
+ratingSelection.forEach(rating => {
+    rating.addEventListener('click', () =>{
+        finalRating.innerText = rating.innerText
+    })
+})
+
+submitButton.addEventListener('click', () =>{
+    thanksCard.classList.remove('hidden');
+    card.classList.add('hidden');
+});
